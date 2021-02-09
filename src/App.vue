94dbcc4bacdf26928a8016b669453120dfbda3c3
Night Wind <lyh1147557099@outlook.com>
2021-02-09 18:45:44 +0800

    Update README.md

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <router-view/>
+  <div class="home">
+    <transition name="fade">
+      <router-view :key="$route.path"/>
+    </transition>
     <link rel="stylesheet" href="https://cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.css">
     <div>
       <mu-container class="container" color="green">
-        <mu-bottom-nav :value="bottomNav">
-          <mu-bottom-nav-item title="接单大厅" icon="home" to="/help_center" value="help_center"></mu-bottom-nav-item>
-          <mu-bottom-nav-item title="发布任务" icon="location_on" to="/job_offer" value="job_offer"></mu-bottom-nav-item>
-          <mu-bottom-nav-item title="个人中心" icon="people" to="/account" value="account"></mu-bottom-nav-item>
+        <mu-bottom-nav >
+          <mu-bottom-nav-item title="接单大厅" icon="home" to="/help_center" ></mu-bottom-nav-item>
+          <mu-bottom-nav-item title="发布任务" icon="location_on" to="/job_offer" ></mu-bottom-nav-item>
+          <mu-bottom-nav-item title="个人中心" icon="people" to="/account" ></mu-bottom-nav-item>
         </mu-bottom-nav>
       </mu-container>
     </div>
@@ -16,30 +18,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      bottomNav: 'help_center'
-    }
-  },
-  created() {
-    this.checkNav()
-  },
-  watch: {
-    '$route': 'checkNav'
-  },
-  methods: {
-    checkNav() {
-      if (this.$route.path === 'help_center') {
-        this.bottomNav = 'help_center'
-      }
-      if (this.$route.path === 'job_offer') {
-        this.bottomNav = 'job_offer'
-      }
-      if (this.$route.path === 'account') {
-        this.bottomNav = 'account'
-      }
-    }
-  }
 
 }
 
@@ -68,5 +46,13 @@ export default {
     margin: 0 auto;
     display: block;
   }
+}
+.fade-enter.active,
+.fade-leave.active{
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to{
+  opacity: 0;
 }
 </style>
